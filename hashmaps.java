5f4a1a073519cc9dad2d2aa144f49c447bd469af
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2021-01-16 20:14:16 +0530

    hashmap basics

---
+++ hashmaps.java
@@ -0,0 +1,41 @@
+package hashMaps;
+
+import java.util.HashMap;
+
+public class hashmaps {
+
+    public static void main(String[] args) {
+        HashMap<String,String> h = new HashMap<String, String>();
+
+        h.put("Shreya","Jain");
+        h.put("Anku","Jain");
+
+        System.out.println(h);
+
+       // System.out.println(h.get("Shreya"));
+
+        HashMap<String, Integer> rollNo = new HashMap<String, Integer>();
+
+        rollNo.put("S", 231);
+        rollNo.put("V", 247);
+        rollNo.put("H", 683);
+
+        for(String i : rollNo.keySet()){
+            System.out.println(i + " has roll number " + rollNo.get(i));
+        }
+
+        rollNo.remove("S");
+
+
+        System.out.println("After removing:");
+
+        for(String i : rollNo.keySet()){
+            System.out.println(i + " has roll number " + rollNo.get(i));
+        }
+        for(String i : h.values()){
+            System.out.println(i);
+        }
+
+    }
+
+}
