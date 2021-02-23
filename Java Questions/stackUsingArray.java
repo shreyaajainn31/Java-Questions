c6f78197ebd149716670af1ecaa74651e5fd5e72
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2021-02-23 15:42:55 +0530

    for positive integers

---
+++ Java Questions/stackUsingArray.java
@@ -0,0 +1,119 @@
+package stacks;
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class stackUsingArray<i> {
+
+    int length;
+    int array[];
+    stackUsingArray(int length){
+        this.length = length;
+        //Emptying the array.
+        array = new int[length];
+        for(int i = 0; i<length; i++){
+            array[i] = -1;
+        }
+
+    }
+
+    int currCount = 0;
+    public void insert(int data){
+
+        array[currCount] = data;
+        currCount++;
+
+        int newArray[] = new int[length * 2];
+
+        for(int i = 0; i<newArray.length; i++){
+            newArray[i] = -1;
+        }
+
+        if(currCount ==  length){
+
+            for(int i = 0; i<length;i++){
+                newArray[i] = array[i];
+            }
+
+            array = newArray;
+        }
+    }
+
+    public void pop(){
+        array[currCount - 1] = -1;
+
+        try{
+            if(currCount >= 0){
+                currCount--;
+            }
+        }
+        catch (Exception e){
+            System.out.println("stack is empty");
+        }
+
+    }
+
+    public int peek(){
+
+        if(array[0] == -1){
+            return -1;
+        }
+        else{
+            int peekCount = currCount;
+            while(peekCount >=0 && array[peekCount] == -1){
+                peekCount--;
+            }
+            return array[peekCount];
+        }
+    }
+
+    public void displayStack(){
+
+        for(int i = array.length - 1; i>=0; i--){
+            if(array[i] != -1){
+                System.out.print(array[i] + " ");
+            }
+        }
+
+        System.out.println();
+    }
+
+    public boolean isEmpty(){
+
+        if(array[0] == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        stackUsingArray stack = new stackUsingArray(5);
+
+        stack.insert(10);
+        stack.insert(20);
+        stack.insert(30);
+        stack.insert(40);
+        stack.insert(50);
+        stack.insert(60);
+
+        stack.pop();
+        stack.insert(30);
+        stack.displayStack();
+        stack.pop();
+        stack.displayStack();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.displayStack();
+        stack.pop();
+        stack.pop();
+        stack.displayStack();
+        stack.pop();
+
+    }
+
+}
